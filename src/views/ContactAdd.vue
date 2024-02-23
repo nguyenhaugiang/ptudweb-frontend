b9c0ebd29aa89ae257c15ee5c7746308ad270f26
B2014563 <giangb2014563@student.ctu.edu.vn>
2024-02-23 15:46:51 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="page justify-content-center align-items-center">
+        <h4>Them moi lien he</h4>
+        <ContactForm />
+    </div>
+</template>
+<script>
+import ContactForm from '@/components/ContactForm.vue';
+import Swal from 'sweetalert2';
+
+export default {
+    components: {
+        ContactForm,
+    },
+    
+    created() {
+        let timerInterval = 500;
+        Swal.fire({
+            title: 'Loading...',
+            timer: 500,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            },
+        });
+    },
+}
+</script>
